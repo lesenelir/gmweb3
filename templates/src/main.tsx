8bec624c665bc69d85ee4302a70421f81b09ecd4
lesenelir <hi@lesenelir.me>
2023-10-04 19:05:04 +0800

    feat: add rainbow connect button

--- templates/src/main.tsx
+++ templates/src/main.tsx
@@ -1,12 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { publicProvider } from 'wagmi/providers/public'
+import { configureChains, createConfig, WagmiConfig } from 'wagmi'
+import { mainnet, polygon, optimism, arbitrum, base, zora } from 'wagmi/chains'
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
 
 import App from '@/pages'
 
 import './index.css'
+import './polyfills'
+import '@rainbow-me/rainbowkit/styles.css'
+
+const {chains, publicClient} = configureChains(
+  [mainnet, polygon, optimism, arbitrum, base, zora],
+  [publicProvider()]
+)
+
+const {connectors} = getDefaultWallets({
+  appName: 'RainbowKit demo',
+  projectId: 'YOUR_PROJECT_ID',
+  chains
+})
+
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App/>
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <App />
+      </RainbowKitProvider>
+    </WagmiConfig>
   </React.StrictMode>
 )
