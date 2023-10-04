75d1e8f837d6e725cb95c9430c0c0b42a0c2f622
lesenelir <hi@lesenelir.me>
2023-10-04 20:50:10 +0800

    feat: add rollup to bundle ts

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+import typescript from '@rollup/plugin-typescript'
+
+export default {
+  input: "src/cli.ts",
+  output: {
+    dir: "dist",
+    format: "es",
+    sourcemap: true
+  },
+  plugins: [typescript({exclude: ['templates/**']})]
+}
